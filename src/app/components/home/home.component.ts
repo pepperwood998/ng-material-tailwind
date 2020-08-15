198e6c9bdbc37955fe13a9e354c8a060af5070c8
zuratama <tuandt66742@gmail.com>
2020-08-15 22:50:07 +0700

    Init first commit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  search: string;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  clear(): void {
+    this.search = '';
+  }
+}
---
+++ src/app/components/share/share.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatBadgeModule } from '@angular/material/badge';
+
+const MODULES = [
+  CommonModule,
+  FormsModule,
+  // Material modules
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatIconModule,
+  MatDividerModule,
+  MatBadgeModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: MODULES,
+  exports: MODULES
+})
+export class ShareModule {}
